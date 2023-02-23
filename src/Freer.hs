061dd6120ba28af5278248af7ce6b25bb87faf37
Samantha Frohlich <sf16540@my.bristol.ac.uk>
2023-02-23 16:23:05 +0000

    fan out thoughts

--- src/Freer.hs
+++ src/Freer.hs
@@ -43,7 +43,7 @@ import Control.Monad (ap, guard, (>=>))
 import Control.Monad.Logic (Logic, MonadLogic (interleave, (>>-)), observeAll)
 import Data.Bifunctor (Bifunctor (..))
 import Data.Char (chr, ord)
-import Data.List (group, minimumBy, sort, transpose)
+import Data.List (group, minimumBy, maximumBy, sort, transpose)
 import Data.Maybe (fromMaybe, listToMaybe, mapMaybe, maybeToList)
 import Data.Monoid (First)
 import Data.Ord (comparing)
@@ -583,6 +583,38 @@ hypoTree =
         r <- comap nodeRight hypoTree
         exact (Node l 0 r)
     ]
+
+-- fan out:
+
+-- Reflective generator that deliberately has a fan out of one
+fanOut1 :: Reflective Int Int
+fanOut1 = exact 1 -- It just generates one, one way
+
+-- Reflective generator that deliberately has a fan out of two
+fanOut2 :: Reflective Int Int
+fanOut2 = oneof [exact 1, exact 1] -- It just generates one, two ways
+
+-- Reflective generator that deliberately has a max fan out of one
+fanOut1' :: Reflective Int Int
+fanOut1' = oneof [exact 1, exact 2] -- It just generates one, one way, and two one way
+
+-- Reflective generator that deliberately has a max fan out of two
+fanOut2' :: Reflective Int Int
+fanOut2' = oneof [exact 1, exact 1, exact 2] -- It just generates one, two ways, and two one way
+
+-- Find the max fan out of a reflective
+-- becomes slow for bst (1,7), taking about three second, says fan out of 1
+findMaxFO :: (Ord b) => Reflective a b -> Int
+findMaxFO = maximum . fmap length . group . sort . enum
+
+-- Find the max fan out of a reflective and which value has that
+findMaxFO' :: (Ord b) => Reflective a b -> (Int, b)
+findMaxFO' = maximumBy (\(x,_) (y,_) -> compare x y) . fmap (\xs -> (length xs, head xs))
+           . group . sort . enum
+
+-- the above are obviously too slow to be tractable on any read reflectives, but
+-- the purpose of a property is to clearly explain what something is, not implement
+-- it efficiently
 
 infFanOut :: Reflective [Int] [Int]
 infFanOut =
